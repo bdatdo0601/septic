9a5cf22efefc72b63d74fedf9df52d0c48000b2a
Dat Bac Do <dat.b.do@gmail.com>
2018-10-12 12:10:50 -0400

    update so date will be grouped

--- src/components/FucksChart/index.ts
+++ src/components/FucksChart/index.ts
@@ -2,6 +2,7 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import { mixins } from "vue-class-component";
 import { FuckHistoryElement } from "@/lib/types/fucks";
 import { Line } from "vue-chartjs";
+import _ from "lodash";
 
 @Component
 export default class FucksChart extends mixins(Line) {
@@ -9,14 +10,31 @@ export default class FucksChart extends mixins(Line) {
     @Prop({ default: 0 }) private currentFucksGiven!: number;
     @Prop({ default: true }) private loading!: boolean;
 
+    private groupData(fucksHistoryData: FuckHistoryElement[]) {
+        if (!this.fucksHistoryData) { return []; }
+        const groupedFucksHistoryData: FuckHistoryElement[] = [];
+        fucksHistoryData.forEach((element: FuckHistoryElement) => {
+            const existedElementIndex = groupedFucksHistoryData.findIndex(
+                (value: FuckHistoryElement) => element.timeframe === value.timeframe,
+            );
+            if (existedElementIndex === -1) {
+                groupedFucksHistoryData.push(element);
+            } else {
+                groupedFucksHistoryData[existedElementIndex].amount += element.amount;
+            }
+        });
+        return groupedFucksHistoryData;
+    }
+
     private displayChart() {
         if (this.loading) { return; }
-        const formattedData = this.fucksHistoryData && this.fucksHistoryData.length > 0 ? {
-            labels: this.fucksHistoryData.map((element, index) => {
+        const groupedData = this.groupData(this.fucksHistoryData);
+        const formattedData = groupedData.length > 0 ? {
+            labels: groupedData.map((element: FuckHistoryElement, index: number) => {
                 if (index === 0) { return "Beginning of Time"; }
                 return element.timeframe;
             }).concat(["Now"]),
-            data: this.fucksHistoryData.map((element) => element.amount).concat([this.currentFucksGiven]),
+            data: groupedData.map((element: FuckHistoryElement) => element.amount).concat([this.currentFucksGiven]),
         } : {
                 labels: ["Beginning of Time", "Now"],
                 data: [0, this.currentFucksGiven],
